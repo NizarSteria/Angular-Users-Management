fdda46209a50c91f5cba6bbc473134d4ed889411
Nizar Aouissaoui <aouissaoui.nizar@gmail.com>
2017-01-01 18:31:47 +0100

    initialisation de projet angular 2

---
+++ README.md
@@ -0,0 +1,29 @@
+Angular2-cli-gestion-utilisateur
+========================
+# Consignes
+
+Migration d'une application de gestion des utilisateurs (d'angular 1 à angular 2)
+
+## Angular Concepts Covered
+
+* TypeScript version that relies on classes and modules
+* Modules are loaded with System.js
+* Defining routes (including child routes)
+* Using Custom Components
+* Using Custom Directives
+* Using Custom Pipes
+* Defining Properties and Using Events in Components/Directives
+* Using the Http object for Ajax calls along with Rx observables
+* Working with Utility and Service classes (such as for sorting and Ajax calls)
+* Using Angular databinding Syntax [], () and [()]
+* Using Angular forms functionality for capturing and validating data
+
+## Running the Application
+
+1. Install `Node.js 6.x` or higher. The server uses ES2015 features so you need Node 6.x or higher!
+
+1. Run `npm install` to install app dependencies
+
+1. Run `npm start` in a separate terminal window to build the TypeScript, watch for changes and launch the web server
+
+1. Go to http://localhost:9000 in your browser
---
+++ server.js
@@ -0,0 +1,32 @@
+var express     = require('express'),
+    bodyParser  = require('body-parser'),
+    fs          = require('fs'),
+    app         = express(),
+    users   = JSON.parse(fs.readFileSync('server/data/users.json', 'utf-8')),
+    states      = JSON.parse(fs.readFileSync('server/data/states.json', 'utf-8'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Would normally copy necessary scripts into src folder (via grunt/gulp) but serving
+//node_modules directly to keep everything as simple as possible
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+//The src folder has our static resources (index.html, css, images)
+app.use(express.static(__dirname + '/src'));
+
+// redirect all others to the index (HTML5 history)
+app.all('/*', function(req, res) {
+    res.sendFile(__dirname + '/src/index.html');
+});
+
+app.listen(3000);
+
+console.log('Express listening on port 3000.');
+
+//Open browser
+// var opn = require('opn');
+
+// opn('http://localhost:3000').then(() => {
+//     console.log('Browser closed.');
+// });
